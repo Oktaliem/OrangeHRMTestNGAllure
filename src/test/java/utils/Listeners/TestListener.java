c80030d893f0bb32b0936d810223a6eed5f40a64
Oktaliem <surya_diharja@yahoo.co.id>
2019-06-07 19:10:27 +0800

    refactor simplified

--- src/test/java/utils/Listeners/TestListener.java
+++ src/test/java/utils/Listeners/TestListener.java
@@ -4,7 +4,6 @@ import com.automation.remarks.video.recorder.VideoRecorder;
 import com.ohrm.utilities.Log;
 import features.Preparation;
 import io.qameta.allure.Attachment;
-import org.awaitility.Awaitility;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -14,7 +13,6 @@ import org.testng.ITestResult;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import static com.google.common.io.Files.toByteArray;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -43,8 +41,8 @@ public class TestListener extends Preparation implements ITestListener {
     }
 
     @Attachment(value = "Video record", type = "video/avi")
-    static byte[] attachVideo(String directory) throws IOException {
-        await().atMost(5, SECONDS); // wait until last video is ready in the folder
+    static byte[] attachVideo(String directory, String methodName) throws IOException {
+        //await().atMost(5, SECONDS); // wait until last video is ready in the folder
         System.out.println("Informasi isFile: "+ VideoRecorder.getLastRecording().isFile());
         System.out.println("Informasi getAbsolutePath: "+ VideoRecorder.getLastRecording().getAbsolutePath());
         System.out.println("Informasi canRead: "+ VideoRecorder.getLastRecording().canRead());
@@ -101,8 +99,32 @@ public class TestListener extends Preparation implements ITestListener {
         }
 */
 
+        byte[] video = new byte[0];
+        breakForLoop:
+        for (int i = 1; i < 5; i++) {
+            if (VideoRecorder.getLastRecording().getAbsolutePath().contains(methodName)){
+                System.out.println("Trial " + i +" "+methodName);
+                video = toByteArray(VideoRecorder.getLastRecording());
+                System.out.println("Video attachment upload status ok" );
+                break breakForLoop;
+            }
+            await().atMost(5, SECONDS);
+        }
+        return video;
+
+/*
         try {
-            return toByteArray(VideoRecorder.getLastRecording());
+            byte[] video = new byte[0];
+            breakForLoop:
+            for (int i = 1; i < 5; i++) {
+                if (VideoRecorder.getLastRecording().getAbsolutePath().contains(methodName)){
+                    System.out.println("Trial " + i +" "+methodName);
+                    video = toByteArray(VideoRecorder.getLastRecording());
+                    break breakForLoop;
+                }
+                await().atMost(5, SECONDS);
+            }
+            return video;
         } catch (IOException e) {
             System.out.println("Unable to attach video, retry attaching the video......");
             File dir = new File(System.getProperty("user.dir") + "\\" + directory);
@@ -118,11 +140,22 @@ public class TestListener extends Preparation implements ITestListener {
                 }
             }
             System.out.println("Last modified video name: " + lastModifiedFile);
-            return toByteArray(lastModifiedFile);
-        }finally {
-            VideoRecorder.getLastRecording().delete();
-        }
 
+            byte[] video = new byte[0];
+            breakForLoop:
+            for (int i = 1; i < 5; i++) {
+                if (lastModifiedFile.toString().contains(methodName)){
+                    System.out.println("Trial" + i);
+                    video = toByteArray(VideoRecorder.getLastRecording());
+                    break breakForLoop;
+                }
+                await().atMost(5, SECONDS);
+            }
+            return video;
+        }finally {
+            //VideoRecorder.getLastRecording().delete();
+        }
+*/
     }
 
     @Attachment(value = "Visual test (screenshot from base image differ)", type = "image/png")
@@ -162,7 +195,6 @@ public class TestListener extends Preparation implements ITestListener {
     @Override
     public void onTestFailure(ITestResult iTestResult) {
         System.out.println("I am in onTestFailure method " + getTestMethodName(iTestResult) + " failed");
-        Awaitility.setDefaultPollInterval(4000, TimeUnit.MILLISECONDS); // wait until last video is ready in the folder
         Object testClass = iTestResult.getInstance();
         WebDriver driver = ((Preparation) testClass).getDriver();
         if (driver instanceof WebDriver) {
@@ -173,7 +205,7 @@ public class TestListener extends Preparation implements ITestListener {
                 getScreenshotDiffer(iTestResult.getTestContext().getAttribute("diff"));
             }
             System.out.println("Video captured for test case:" + getTestMethodName(iTestResult));
-            try { attachVideo("video"); } catch (IOException e) { e.printStackTrace();}
+            try { attachVideo("video",getTestMethodName(iTestResult) ); } catch (IOException e) { e.printStackTrace();}
         }
         saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");
         String folder = System.getProperty("user.dir");
